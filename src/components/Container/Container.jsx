531853b33b1caa006152e7f64680a34d89b90c7d
Evgeniy <evg1999prog@gmail.com>
2024-12-19 22:37:02 +0200

    header & container

---
+++ src/components/Container/Container.jsx
@@ -0,0 +1,8 @@
+import classNames from "classnames";
+import style from "./Container.module.css";
+
+export const Container = ({children, className}) => {
+    return (
+        <div className={classNames(style.container, className)}>{children}</div>
+    );
+};
