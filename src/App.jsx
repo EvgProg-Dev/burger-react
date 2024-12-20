baef1056f5f3bb08a05e7c5a05e4773d9c7ea049
Evgeniy <evg1999prog@gmail.com>
2024-12-20 15:54:06 +0200

    redux, category slice

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,17 @@
+import { Provider } from "react-redux";
 import { Catalog } from "./components/Catalog/Catalog";
 import { Header } from "./components/Header/Header";
 import { Navigation } from "./components/Navigation/Navigation";
+import { store } from "./Store";
 
 export const App = () => {
     return (
-        <>
+        <Provider store={store}>
             <Header />
             <main>
-              <Navigation />
-              <Catalog />
+                <Navigation />
+                <Catalog />
             </main>
-        </>
+        </Provider>
     );
 };
