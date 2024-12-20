0618144b467d98815d8e3a8b1c395f3f9297a3a8
Evgeniy <evg1999prog@gmail.com>
2024-12-20 15:05:06 +0200

    Fixed Count Component

--- src/components/Count/Count.jsx
+++ src/components/Count/Count.jsx
@@ -1,11 +1,29 @@
+import { useState } from "react";
 import style from "./Count.module.css";
 
-export const Count = ({ count }) => {
+export const Count = (props) => {
+    const [count, setCount] = useState(props.count);
+
+    const addCount = () => {
+        setCount(count + 1);
+    };
+    const removeCount = () => {
+        count > 1 && setCount(count - 1);
+    };
+
     return (
         <div className={style.count}>
-            <button className={style.minus}>-</button>
+            <button
+                className={style.minus}
+                onClick={removeCount}
+                disabled={count === 1}
+            >
+                -
+            </button>
             <p className={style.amount}>{count}</p>
-            <button className={style.plus}>+</button>
+            <button className={style.plus} onClick={addCount}>
+                +
+            </button>
         </div>
     );
 };
