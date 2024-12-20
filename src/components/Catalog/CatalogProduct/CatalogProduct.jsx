d490d4fb6b8b0cfa50c88c470e757307a9b7a7db
Evgeniy <evg1999prog@gmail.com>
2024-12-20 23:00:56 +0200

    orderSlice

--- src/components/Catalog/CatalogProduct/CatalogProduct.jsx
+++ src/components/Catalog/CatalogProduct/CatalogProduct.jsx
@@ -1,10 +1,17 @@
+import { useDispatch } from "react-redux";
 import { API_URI } from "../../../const";
 import style from "./CatalogProduct.module.css";
+import { addProduct } from "../../../Store/order/orderSlice";
 
-export const CatalogProduct = ({ image, price, title, weight }) => {
+export const CatalogProduct = ({ id, image, price, title, weight }) => {
+    const dispatch = useDispatch();
     return (
         <article className={style.product}>
-            <img src={`${API_URI}/${image}`} alt={title} className={style.image} />
+            <img
+                src={`${API_URI}/${image}`}
+                alt={title}
+                className={style.image}
+            />
             <p className={style.price}>
                 {price}
                 <span className="currency">₽</span>
@@ -13,7 +20,17 @@ export const CatalogProduct = ({ image, price, title, weight }) => {
                 <button className={style.detail}>{title}</button>
             </h3>
             <p className={style.weight}>{weight}г</p>
-            <button className={style.add} type="button">
+            <button
+                className={style.add}
+                type="button"
+                onClick={() =>
+                    dispatch(
+                        addProduct({
+                            id,
+                        })
+                    )
+                }
+            >
                 Добавить
             </button>
         </article>
